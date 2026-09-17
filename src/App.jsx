cd10459c4e881b764b702ae704226180867e84c9
jkatariina <j.katariina@icloud.com>
2026-09-17 13:47:43 +0200

    feat: set up routing with board, import and 404 views

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import BoardPage from './pages/BoardPage.jsx'
+import ImportPage from './pages/ImportPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+
 function App() {
   return (
-    <main>
-      <h1>Sprintboard</h1>
-    </main>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<BoardPage />} />
+          <Route path="/importera" element={<ImportPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
