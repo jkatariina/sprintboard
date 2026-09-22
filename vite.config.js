df47f3141a2d358bd2e8a6e940e93f1312907410
jkatariina <j.katariina@icloud.com>
2026-09-22 13:22:50 +0200

    ci: deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/sprintboard/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-})
+}))
